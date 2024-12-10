6e7e36eb87f744fb672fc902321003d836dc2774
Filipe Camarata <filipeedvandro942@gmail.com>
2024-12-10 19:55:25 -0300

    feat:initial structure

---
+++ index.js
@@ -0,0 +1,9 @@
+//Inportação das bibliotecas
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+//Porta do servidor
+const port = 3000
